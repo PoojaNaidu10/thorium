6b9d7ec2512b7939f138a0777980cbcee59e3db0
Pooja Naidu <poojanaidu772.com>
2022-02-25 02:53:52 +0530

    this is the post-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,46 +1,113 @@
-
 const express = require('express');
-
 const router = express.Router();
-
-router.get('/test-me', function (req, res) {
-    
-    res.send('My first ever api!')
-});
-
-router.get('/movies', function (req, res){
-    res.send('["abc","def","ghi","jkl","mno"]')
-});
-
-router.get('/movies/:movieId', function(req, res){
-    mov=["abc","def","ghi","jkl","mno"]
-    let value = req.params.movieId;
-    if(value>mov.length-1){
-        res.send('"doesnt exist"')
-    }else{
-        res.send(mov[value])
+ 
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ],
+           "bookings":[
+               {
+                   "bookingNumber":1,
+                   "sportId":"",
+                   "centerId":"",
+                   "type":"private",
+                   "slot":"165842594656",
+                   "bookedOn":"25/02/2022",
+                   "bookedFor":"26/02/2022"
+               }
+           ]
+           
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+           "bookings":[
+            {
+                "bookingNumber":2,
+                "sportId":"",
+                "centerId":"",
+                "type":"private",
+                "slot":"165842594656",
+                "bookedOn":"25/02/2022",
+                "bookedFor":"26/02/2022"
+            }
+        ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+           "bookings":[
+            {
+                "bookingNumber":3,
+                "sportId":"",
+                "centerId":"",
+                "type":"private",
+                "slot":"165842594656",
+                "bookedOn":"25/02/2022",
+                "bookedFor":"26/02/2022"
+            }
+        ]
+       },
+   ]
+ 
+   router.post('/players', function (req, res) {
+ 
+    let player = req.body;
+    let playerName = player.name
+    for (let i = 0; i < players.length; i++){
+        if(players[i].name == playerName){
+            res.send('Player already exist')
+        }
     }
-});
+    players.push(player);
+   console.log("Here is the player array", players);
+    res.send(players);
+   });
+   router.post('/players/:playerName/bookings/:bookingId', function (req, res) {
+    let name = req.params.playerName;
+    let isPlayerPresent = false;
+    for (let i = 0; i < players.length; i++) {
+        if (players[i].name == name) {
+            isPlayerPresent = true;
+        }
+    }
+    if (!isPlayerPresent) {
+        res.send('Player not present');
+    }
+    res.send('Player is present')
 
-router.get('/moviez', function (req, res){
-    res.send([ {id: 1,name:'abc'}, {id: 2,name:'def'},{id: 3,name:'ghi'},{id: 4,name:'jkl'},{id: 5,name:'mno'}])
-});
-
-router.get('/films/:filmId', function(req, res){
-   let movi = ([{id: 1,name:'abc'}, {id: 2,name:'def'},{id: 3,name:'ghi'},{id: 4,name:'jkl'},{id: 5,name:'mno'}])
-   let value = req.params.filmId;
-   let found=false;
-   for(i=0;i<movi.length;i++){
-       if(movi[i].id==value){
-           found=true
-           res.send(movi[i])
-           break;
-       }
-   }
-   if(found==false){
-       res.send('No movie exist with this id')
-   }
-
+    let booking = req.body;
+    let bookingId = req.params.bookingId;
+    for (let i = 0; i < players.length; i++) {
+        if (players[i].name == name) {
+            for (let j = 0; j < players[i].bookings.length; j++) {
+                if (players[i].bookings[j].bookingNumber == bookingId) {
+                   return res.send('Booking with this id is already present for the player');
+                }
+            }
+            players[i].bookings.push(booking);
+        }
+    }
+    res.send(players);
 })
 
+  
 module.exports = router;
+
+  
