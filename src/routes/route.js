9527e792113433bf30ccc004775c2fb60ab8a498
Pooja Naidu <poojanaidu772.com>
2022-02-26 15:19:28 +0530

    query-param-file

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,45 +3,45 @@ const router = express.Router();
 
 // QUERY PARAMS
 // localhost:3000/get-query-1?myCoolVar=24&xyz=hiFunctionUP
-router.get("/get-query-1", function (req, res) {
-    let data = req.query
-    console.log(data)
-    res.send({ data: data, status: true })
-})
+// router.get("/get-query-1", function (req, res) {
+//     let data = req.query
+//     console.log(data)
+//     res.send({ data: data, status: true })
+// })
 
 
 // take marks in req.query variable named "marks" and send "pass" if > 40 else "fail"
-router.get("/get-query-2", function (req, res) {
-    let marks = req.query.marks
-    // { marks: '80'}
+// router.get("/get-query-2", function (req, res) {
+//     let marks = req.query.marks
+//     // { marks: '80'}
 
-    let result = marks > 40 ? "pass" : "fail"
-    // let result = "fail"
-    // if (marks> 40) { result = "pass" }
-    // // else { result = "fail" }
+//     let result = marks > 40 ? "pass" : "fail"
+//     //let result = "fail"
+//     //if (marks> 40) { result = "pass" }
+//      //else { result = "fail" }
 
-    res.send({ result: result, status: true })
-})
+//     res.send({ result: result, status: true })
+// })
 
 //query params are also available in post request
-router.post("/post-query-1", function (req, res) {
-    let data = req.query
-    console.log(data)
-    res.send({ result: data, status: true })
-})
+// router.post("/post-query-1", function (req, res) {
+//     let data = req.query
+//     console.log(data)
+//     res.send({ result: data, status: true })
+// })
 
-let myArr = [23, 45, 67, 281394, 32424, 423, 24, 42323, 4, 234, 12, 34]
+//let myArr = [23, 45, 67, 281394, 32424, 423, 24, 42323, 4, 234, 12, 34]
 //filter out all the numbers that are greater than input( input is received from query params)
-router.post("/post-query-2", function (req, res) {
+//router.post("/post-query-2", function (req, res) {
     //CODE HERE
-    let input= req.query.input
-    let finalArr= myArr.filter( ele => ele > input)
+    //let input= req.query.input
+//let finalArr= myArr.filter( ele => ele > input)
     // let finalArr=[]
     // for (i=0 ; i<myArr.length; i++) {
     //     if ( myArr[i] > input )      finalArr.push(myArr[i]) 
     // }
-    res.send({ result: finalArr , status: true })
-})
+   // res.send({ result: finalArr , status: true })
+//})
 
 
 // ASSIGNMENT:
@@ -49,34 +49,44 @@ router.post("/post-query-2", function (req, res) {
 // take input in query param as votingAge..and for all the people above that age, change votingStatus as true
 // also return an array consisting of only the person that can vote
 
-//  take this as sample for array of persons:
-// let persons= [
-//     {
-//     name: "PK",
-//     age: 10,
-//     votingStatus: false
-// },
-// {
-//     name: "SK",
-//     age: 20,
-//     votingStatus: false
-// },
-// {
-//     name: "AA",
-//     age: 70,
-//     votingStatus: false
-// },
-// {
-//     name: "SC",
-//     age: 5,
-//     votingStatus: false
-// },
-// {
-//     name: "HO",
-//     age: 40,
-//     votingStatus: false
-// }
-// ]
+// take this as sample for array of persons:
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+},
+{
+    name: "SK",
+    age: 20,
+    votingStatus: false
+},
+{
+    name: "AA",
+    age: 70,
+    votingStatus: false
+},
+{
+    name: "SC",
+    age: 5,
+    votingStatus: false
+},
+{
+    name: "HO",
+    age: 40,
+    votingStatus: false
+}
+]
+
+router.post("/post-query", function (req, res) {
+let votingAge = req.query.votingAge 
+let person = persons.filter(element => element.age > votingAge)
+console.log(person)
+
+let update = person.map(element => ({...element,votingStatus : true}))
+res.send(update)
+
+});
 
 
 
